c774144592df87a71932aa1b869d998f7ab6198d
Cory Mortimer Martin <cory.m.mortimer@gmail.com>
2021-05-02 16:22:24 -0700

    Add wrapper around axios and react query

---
+++ src/utils/http/http.jsx
@@ -0,0 +1,15 @@
+import { useQuery, QueryClient, QueryClientProvider } from "react-query";
+export { default } from "axios";
+
+const queryClient = new QueryClient();
+export const useHttpQuery = (...params) => {
+  const result = useQuery(...params);
+  return { ...result, response: extractData(result) };
+};
+export const HttpCache = ({ children }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
+
+export const extractData = (data) => {
+  return data?.data?.data;
+};
