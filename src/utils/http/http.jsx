5f34e6b83902334b052d1502d46ede75d6d64263
Cory Mortimer Martin <cory.m.mortimer@gmail.com>
2021-05-04 20:04:50 -0700

    Cache indefinitely

--- src/utils/http/http.jsx
+++ src/utils/http/http.jsx
@@ -1,11 +1,19 @@
 import { useQuery, QueryClient, QueryClientProvider } from "react-query";
 export { default } from "axios";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+    },
+  },
+});
+
 export const useHttpQuery = (...params) => {
   const result = useQuery(...params);
   return { ...result, response: extractData(result) };
 };
+
 export const HttpCache = ({ children }) => (
   <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 );
